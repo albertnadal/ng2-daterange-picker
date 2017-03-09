8334411227176d77dfa514bf58fb01afacca15f6
Albert Nadal Garriga <albert@albert.local>
2017-03-09 10:22:44 +0100

    Fixed important bug in the module configuration file.

--- src/daterange.picker.module.ts
+++ src/daterange.picker.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 
 import { DaterangePickerComponent } from './ang.daterange.picker.component';
+import { DatePickerComponent } from './ang.datepicker.component';
 
 export * from './ang.daterange.picker.component';
 
 @NgModule({
-  declarations: [ DaterangePickerComponent ],
+  declarations: [ DaterangePickerComponent, DatePickerComponent ],
   exports: [ DaterangePickerComponent ],
   imports: [ CommonModule, FormsModule, ReactiveFormsModule, MaterialModule ]
 })
